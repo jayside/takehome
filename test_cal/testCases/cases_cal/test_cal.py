d24e20b28ca3ab4b2bca96bf4bf0bd07891fcf4e
jayside <104363857+jayside@users.noreply.github.com>
2022-06-09 09:13:12 +0800

    Update test_cal.py
    删除测试代码

--- test_cal/testCases/cases_cal/test_cal.py
+++ test_cal/testCases/cases_cal/test_cal.py
@@ -7,9 +7,9 @@ from test.calculate import Calculate
 from test_cal.libs.get_yaml import YAMLUtils
 
 
-class TestCase001:
+class TestCal:
     datas = YAMLUtils.utils_get_test_data('test_cal.yml', 'test_cal_01')
-    ww = ['dd','as','11','aa']
+
     @pytest.mark.parametrize('datas', datas[1], ids=datas[0])
     def test_cal_01(self, datas):
         re = Calculate()
